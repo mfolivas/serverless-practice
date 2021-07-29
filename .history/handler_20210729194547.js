762fdcdf62fcd9272c2abcf8307eb4f2f5f39d6b
Marcelo Olivas <mfolivas@gmail.com>
2021-07-29 19:47:07 -0400

    added logic

---
+++ .history/handler_20210729194547.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports.hello = async (event) => {
+  console.log('HERE! HERE!', event)
+  const { total, amount, clientName } = event.body
+
+  // total should be greater than 1
+  const 
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
---
+++ .history/handler_20210729194625.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports.hello = async (event) => {
+  console.log('HERE! HERE!', event)
+  const { total, amount, clientName } = event.body
+
+  const id = []
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
