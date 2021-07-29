62dfa58d9bbad7d578ba29f64e6b18bb7d7bacf1
Marcelo Olivas <mfolivas@gmail.com>
2021-07-29 19:50:38 -0400

    added small app

--- handler.js
+++ handler.js
@@ -1,23 +1,54 @@
-'use strict';
+const AWS = require('aws-sdk')
+const { unmarshall } = AWS.DynamoDB.Converter
+const DynamoDB = new AWS.DynamoDB.DocumentClient()
 
-module.exports.hello = async (event) => {
+module.exports.addOrder = async (event) => {
   console.log('HERE! HERE!', event)
-  const { total, amount, clientName } = event.body
-
-  const id = []
+  const { total, amount, client } = JSON.parse(event.body)
+  const now = new Date()
+  const clientName = client.name
+  const id = [clientName, now.toISOString()].join('-')
+  // Dynamo stuff goes here
+  const params = {
+    // TODO this doesn't look good (hardcoded)
+    TableName: 'orders-dev',
+    Item: {
+      id,
+      total,
+      amount,
+      clientName
+    }
+  }
+  await DynamoDB.put(params).promise()
 
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+        id
+      }
+    )
+  }
+}
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+
+module.exports.getOrder = async (event) => {
+  console.log('GET ORDER!', event)
+  const { id } = event.pathParameters
+  const params = {
+    TableName: 'orders-dev',
+    Key: { id }
+  }
+  const result = await DynamoDB.get(params).promise()
+  const { total, amount, clientName } = result.Item
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        total,
+        amount,
+        clientName
+      }
+    )
+  }
+}
